b4f5f61bbfbaf511942438ab45fcc620df999f75
matthiasdurivet <Matthias.durivet@student.howest.be>
2018-04-12 14:08:41 +0100

    warmth bar works

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ let cursors;
 let camera;
 let waterDrops = [];
 let projectiles = [];
+let sinks = [];
 let outerThis;
 let lastFullPercentage;
 let warmthText;
 let candle;
+let warmthBarBackground;
+let warmthBar;
+let gameEnded = false;
 
 const projectileSpeed = 750;
 const bounce = 0.1;
@@ -51,6 +55,9 @@ function preload ()
     this.load.image('candle_on', 'assets/candle_on.png');
     this.load.image('candle_off', 'assets/candle_off.jpg');
     this.load.image('victory', 'assets/victory.jpg');
+    this.load.image('bar', 'assets/warmth_bar.jpg');
+    this.load.image('bar_back', 'assets/black.jpg');
+    this.load.image('sink', 'assets/sink.png');
 }
 
 function create ()
@@ -67,24 +74,33 @@ function create ()
     player.setBounce(bounce);
     player.setCollideWorldBounds(true);
     player.warmth = 100;
-    warmthText = this.add.text(width*4/10, height/10, 'Warmth: 100', { fill: '#ffffff', font: '18pt Arial' });
-    warmthText.setScrollFactor(0);
+
+    warmthBarBackground = this.add.image(width/2, height/10, 'bar_back').setDisplaySize(404,44).setScrollFactor(0);
+    warmthBarBackground.scaleX = 0.255;
+    warmthBar = this.add.image(width/2, height/10, 'bar').setDisplaySize(400,40).setScrollFactor(0);
+    warmthText = this.add.text(width/2, height/10, 'Warmth: 100', { fill: '#ffffff', font: '18pt Arial' }).setOrigin(0.5,0.5).setScrollFactor(0);
     updateWarmthBar();
     player.body.setGravityY(gravity);
+
     for(let i=0;i<1;i++){
         let drop = this.physics.add.sprite(width/2+ (i*500),height/2,'waterDrop');
         initiateWaterDrop(drop);
         waterDrops.push(drop);
     }
-
+    for(let i=0;i<1;i++){
+        let sink = this.physics.add.sprite(width/2+ (i*500),height/2,'sink').setDisplaySize(200,50);
+        sinks.push(sink);
+    }
 
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(waterDrops, platforms);
     this.physics.add.collider(candle, platforms);
+    this.physics.add.collider(sinks, platforms);
     this.physics.add.overlap(player, projectiles, projectileContact, null, this);
     this.physics.add.overlap(platforms, projectiles, killProjectile, null, this);
     this.physics.add.overlap(player, waterDrops, creatureContact, null, this);
     this.physics.add.overlap(player, candle, lightCandle, null, this);
+    this.physics.add.overlap(player, sinks, sinkCollision, null, this);
 
     //CAMERA STUFF
     camera = this.cameras.main.setSize(width, height);
@@ -96,20 +112,24 @@ function update ()
 {
     const playerSpeed = 300;
     const playerJumpHeight = 1000;
+    const playerSpeedIncrement = 150;
+    const playerFriction = 1.1;
 
-    if(player.warmth<=0) endGame();
+    if(gameEnded) return null;
+
+    if(player.warmth<=0) loseGame();
 
     if (cursors.left.isDown)
     {
-        player.setVelocityX(-playerSpeed);
+        if(player.body.velocity.x>-playerSpeed) player.body.velocity.x -= playerSpeedIncrement
     }
     else if (cursors.right.isDown)
     {
-        player.setVelocityX(playerSpeed);
+        if(player.body.velocity.x<playerSpeed) player.body.velocity.x += playerSpeedIncrement
     }
     else
     {
-        player.setVelocityX(0);
+        player.body.velocity.x = Math.floor(player.body.velocity.x/playerFriction);
     }
 
     if (cursors.up.isDown && player.body.touching.down)
@@ -176,17 +196,20 @@ function initiateWaterProjectile(projectile, left) {
 }
 
 function updateWarmthBar() {
-    warmthText.setText('Warmth: '+ Math.ceil(player.warmth));
+    let warmth = Math.ceil(player.warmth);
+    warmthText.setText('Warmth: '+ warmth);
+    warmthBar.scaleX = warmth/400;
 }
 
 function projectileContact(player,projectile) {
-    player.warmth -= 25;
+    loseWarmth(25);
     projectile.disableBody(true,true);
 }
 
 function creatureContact(player,creature) {
-    player.warmth -= 50;
+    loseWarmth(50);
     kill(creature);
+    player.setVelocityX(-2000);
 }
 
 function killProjectile(projectile,platform) {
@@ -198,14 +221,24 @@ function kill(object) {
     object.active = false;
 }
 
-function endGame() {
+function destroyAll() {
+    waterDrops = [];
+    projectiles = null;
+    sinks = [];
+}
+
+function loseGame() {
     outerThis.add.image(0,0,'gameOver').setOrigin(0).setDisplaySize(width, height);
     camera.setBounds(0, 0, width, height);
+    destroyAll();
+    gameEnded = true;
 }
 
 function winGame() {
     outerThis.add.image(0,0,'victory').setOrigin(0).setDisplaySize(width, height);
     camera.setBounds(0, 0, width, height);
+    destroyAll();
+    gameEnded = true;
 }
 
 function lightCandle() {
@@ -213,4 +246,12 @@ function lightCandle() {
     kill(candle);
     outerThis.physics.add.collider(candleOn, platforms);
     setTimeout(winGame,2000);
+}
+
+function sinkCollision() {
+    loseWarmth(5);
+}
+
+function loseWarmth(amount) {
+    player.warmth -= amount;
 }
